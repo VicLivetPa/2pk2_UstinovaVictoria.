8f2587ad762919a161cc83343a527d121ae7db9b
Victoria Ustinova <144162159+VicLivetPa@users.noreply.github.com>
2023-09-27 17:48:28 +0500

    Add files via upload

---
+++ PZ_02/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PZ_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число m: ");
+            int m = Convert.ToInt32(Console.ReadLine()); //целое число
+            double  n, x, y, z; // действительные числа
+            Console.WriteLine("Введите число n: ");
+            n = double.Parse(Console.ReadLine());
+            m = (int)double.Parse(Console.ReadLine());  
+            if (m > 5)
+            {
+                x = Math.Sin(m + n);
+            }
+            else
+            {
+                x = (n + 5 * Math.Sqrt(m + Math.Pow(n, 2) - 2.1 * n));
+            }
+            if (x > 0)
+            {
+                y = Math.Cos(m + n) * Math.Sin(n * x);
+            }
+            else
+            {
+                y = (Math.Log(m * n + x));
+            }
+            z = (2 * x + 3 * y) / (Math.Pow(m, 2) + 5);
+            Console.WriteLine("Ответ:" + z); // показ ответа
+        }
+    }
+}
