b68ccb8728dffc222b8cb2f7e0f7252d580617f0
VicLivetPa <minapak985@gmail.com>
2023-09-27 17:11:57 +0500

    Добавьте файлы проекта.

---
+++ PZ_06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] array = new double[20];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Math.Pow(2, i) / 2;
+            }
+
+            Array.Reverse(array);
+
+            foreach (double element in array)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
+       
+        
+
