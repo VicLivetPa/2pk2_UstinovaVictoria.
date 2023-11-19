f06cc2b7d5b7cbf2f3da3b641912fc7f6c68f79a
Victoria Ustinova <144162159+VicLivetPa@users.noreply.github.com>
2023-11-19 13:12:26 +0500

    Добавлена 9 практика

---
+++ PZ_09/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PZ_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            int g = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите строку: ");
+                string str = Console.ReadLine();
+                 //првоеряем строку на содержание только Букв
+                if (str.All(char.IsLetter))
+                {
+                    list.Add(str);
+                }
+                 //првоеряем пустая ли строка
+                if (String.IsNullOrEmpty(str))
+                {
+                    Console.WriteLine("Пустая строка");
+                    Console.Write("Первая введеная строка которая не содержит циры: ");
+                    Console.WriteLine(list[0]);
+                    break;
+                }
+            }
+        }
+    }
+}
