9f6158b7a87d11789dcc0d7a51e2c3e6eb44cad8
Victoria Ustinova <144162159+VicLivetPa@users.noreply.github.com>
2023-11-23 02:00:09 +0500

    Добавлена 10 практика

---
+++ PZ_10/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Введите текст:");
+        string text = Console.ReadLine();
+
+        string newText = RemoveContentInBrackets(text);
+        Console.WriteLine("Обновленный текст:");
+        Console.WriteLine(newText);
+    }
+
+    static string RemoveContentInBrackets(string text)
+    {
+        int startIndex;
+        while ((startIndex = text.IndexOf('(')) != -1)
+        {
+            int endIndex = text.IndexOf(')', startIndex);
+            if (endIndex != -1)
+            {
+                text = text.Remove(startIndex, endIndex - startIndex + 1);
+            }
+        }
+               return text;
+        }
+    }
+}
