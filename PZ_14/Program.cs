8f9e9996f63a7ac6cc042769c2faad2f5e1c0bfe
Victoria Ustinova <144162159+VicLivetPa@users.noreply.github.com>
2023-12-03 14:52:54 +0500

    Добавлена 14 практика

---
+++ PZ_14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace PZ_14
+{
+    internal class Program
+    {
+            static void Main()
+            {
+                string filePath = "C:\\Users\\опопппло\\source\\repos\\2pk2_UstinovaVictoria\\2\\2pk2_UstinovaVictoria\\PZ_14\\mmm.txt";
+
+                string[] lines = File.ReadAllLines(filePath);   // Чтение файла
+           
+                 foreach (string line in lines)  // Вывод строк 
+                 {
+                     if (line.EndsWith("?") || line.EndsWith("!")) // оканчивающихся вопросительным или восклицательным знаком
+                     {
+                         Console.WriteLine(line);
+                     }
+                     if (line.EndsWith("."))
+                     {
+                        Console.WriteLine('0');
+                     }
+                     
+                 }
+            } 
+            
+    }
+}
