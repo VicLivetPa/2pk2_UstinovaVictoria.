1bd6b55a4411f204e4287ff0c7df749a486bd3b8
Victoria Ustinova <144162159+VicLivetPa@users.noreply.github.com>
2023-12-20 18:54:34 +0500

    д 15 пз

---
+++ PZ_15/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите полный путь к каталогу: ");
+            string directoryPath = Console.ReadLine();
+
+            if (Directory.Exists(directoryPath))
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
+                DirectoryInfo[] subdirectories = directoryInfo.GetDirectories();
+
+                var sortedSubdirectories = subdirectories.OrderByDescending(subdir => GetDirectorySize(subdir));
+
+                Console.WriteLine("Имена каталогов в порядке убывания по размеру: ");
+
+                foreach (var subdir in sortedSubdirectories)
+                {
+                    Console.WriteLine(subdir.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Указанный каталог не существует...");
+            }
+
+            Console.ReadKey();
+        }
+
+        static long GetDirectorySize(DirectoryInfo directoryInfo)
+        {
+            long size = 0;
+
+            FileInfo[] files = directoryInfo.GetFiles();
+            foreach (var file in files)
+            {
+                size += file.Length;
+            }
+
+            DirectoryInfo[] subdirectories = directoryInfo.GetDirectories();
+            foreach (var subdir in subdirectories)
+            {
+                size += GetDirectorySize(subdir);
+            }
+            return size;
+        }
+    }
+}
