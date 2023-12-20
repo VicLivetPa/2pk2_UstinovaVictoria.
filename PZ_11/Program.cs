41b238ce8a2b1e7789a7e5cad6a60dfd72a29594
Victoria Ustinova <144162159+VicLivetPa@users.noreply.github.com>
2023-12-20 19:16:41 +0500

    д 11 пз

---
+++ PZ_11/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PZ_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int RootsCount(double A, double B, double C)
+            {
+                double discriminant = B * B - 4 * A * C;
+
+                if (discriminant > 0)
+                {
+                    // Два различных корня
+                    return 2;
+                }
+                else if (discriminant == 0)
+                {
+                    // Один корень
+                    return 1;
+                }
+                else
+                {
+                    // Нет корней
+                    return 0;
+                }
+            }
+            // Пример использования 
+            int eRoots = RootsCount(6, -6, 1);//2 корня
+           
+            Console.WriteLine("Уравнение.Кол-во корней: " + eRoots);
+        }
+    }
+}
